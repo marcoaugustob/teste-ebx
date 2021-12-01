e5684e5e04905e88e7eb9ccaea99f49c4f50dde9
Marco A. Bonfim <marco.dextra@dotzmkt.com.br>
2021-12-01 15:47:50 -0300

    teste response

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express()
 
-app.use('/', require('./route/moviesRoute'));
-
+//app.use('/', require('./route/moviesRoute'));
+app.get('/', (req, res) => {
+    res.json({'message': 'ok'});
+  })
 app.listen(3000);
