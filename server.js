3b15af8beb033a4455210d7dc7e8b56a3d0ad81b
Marco A. Bonfim <marco.dextra@dotzmkt.com.br>
2021-12-03 08:33:29 -0300

    update use  express

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 app.get('/', (req, res) => {
     res.json({ 'message': 'ok' });
 })
-
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/', require('./route/moviesRoute'));
